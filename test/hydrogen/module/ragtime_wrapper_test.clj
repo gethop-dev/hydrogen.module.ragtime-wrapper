4cdc15813e342e5e554fb2446784a6acc64bfe18
Damian Hryniewicz <damian@magnet.coop>
2019-12-20 15:57:17 +0100

    Remove the fake failing test

--- test/hydrogen/module/ragtime_wrapper_test.clj
+++ test/hydrogen/module/ragtime_wrapper_test.clj
@@ -1,7 +1,3 @@
 (ns hydrogen.module.ragtime-wrapper-test
   (:require [clojure.test :refer :all]
             [hydrogen.module.ragtime-wrapper :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
