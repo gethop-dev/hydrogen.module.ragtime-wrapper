985be6bed6ce57486847a3219a59459029ab4dc0
spietras <szymon.pietras@magnet.coop>
2022-05-27 14:24:01 +0100

    Bring `hydrogen` back to ns naming pattern
    This library is a `hydrogen` module - while moving `hydrogen`-related
    libraries to HOP organization, we do not want to get rid of `hydrogen`
    name or change it to `dev.gethop` - this is still `hydrogen`.
    What we need is to move the repository to `gethop-dev` Github
    organization and publish releases under `dev.gethop` group. So
    `hydrogen` brand name should remains as it was.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject dev.gethop/module.ragtime-wrapper "0.1.2-SNAPSHOT"
+(defproject dev.gethop/hydrogen.module.ragtime-wrapper "0.1.2-SNAPSHOT"
   :description "Duct module wrapping configuration for Ragtime migrations"
-  :url "https://github.com/gethop-dev/module.ragtime-wrapper"
+  :url "https://github.com/gethop-dev/hydrogen.module.ragtime-wrapper"
   :license {:name "Mozilla Public Licence 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
--- test/hydrogen/module/ragtime_wrapper_test.clj
+++ test/hydrogen/module/ragtime_wrapper_test.clj
@@ -2,5 +2,5 @@
 ;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-(ns dev.gethop.module.ragtime-wrapper-test
+(ns hydrogen.module.ragtime-wrapper-test
   (:require [clojure.test :refer :all]))
